852c0169bbf8e931fdac28271cd072516c68ae86
Dan Sosedoff <dan.sosedoff@gmail.com>
2018-10-23 13:02:46 -0400

    Remove hex.go

--- secret.go
+++ secret.go
@@ -81,3 +81,16 @@ func wrapText(text string) string {
 
 	return string(result)
 }
+
+func hexDecode(input string) (string, error) {
+	input = strings.TrimSpace(input)
+	input = strings.Replace(input, "\r", "", -1)
+	input = strings.Replace(input, "\n", "", -1)
+
+	decoded, err := hex.DecodeString(input)
+	if err != nil {
+		return "", err
+	}
+
+	return string(decoded), nil
+}
